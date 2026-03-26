8f8c7f70b91bac6899995ef04d0aeb7059232558
Chaitanya-666 <orionck545@gmail.com>
2026-03-26 19:11:40 +0530

    neovim basic done

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -40,3 +40,4 @@ keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnos
 keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
